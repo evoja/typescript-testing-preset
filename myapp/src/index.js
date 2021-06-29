819844ba9f40d0015e69a911d11973e1c7470696
chivorotkiv <git@chiv.info>
2021-06-29 12:18:14 +0600

    upgrade myapp to use 0.0.4 version of mylib

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,2 +1,2 @@
-const {add2} = require('@evoja/typescript-testing-preset--lib/tools/add-util')
+const {add2} = require('@evoja/typescript-testing-preset--lib/tools/sum-util')
 console.log(add2(100)) // 102 is expected to be printed
